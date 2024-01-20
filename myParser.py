a6d9225b73ab1800d01dbb5c215721d663b9cc0c
Patrycja Więcław <patrycja.wieclaw@onet.eu>
2024-01-21 00:43:09 +0100

    fixed some jumps and a few other things

--- myParser.py
+++ myParser.py
@@ -6,7 +6,7 @@ import sys
 # ta tablica będzie zawierać wszystkie zadeklarowane symbole w postaci: (nazwa_symbolu, czy_tablica, pojemność_tablicy, miejsce_w_pamięci, czy_zainicjowana)
 # nazwa_procedury tylko, jeżeli zmienna pochodzi z procedury; jeszcze musi być adres pamięci
 symbols_array = []
-# każdy element procedures_list będzie postaci (nazwa_procedury, gdzie_początek, gdzie_koniec)
+# każdy element procedures_list będzie postaci (nazwa_procedury, gdzie_początek, gdzie_koniec, adres_assemblerowy) - main też musi być tu dodany
 procedures_list = []
 first_free_mem_index = 0
 # curr_line_in_code - obecna linijka w generowanym kodzie, czy to zadziała???
@@ -16,7 +16,6 @@ curr_line_in_code = 0
 
 # funkcja dodająca nową nazwę zmiennej do zbioru już zadeklarowanych zmiennych
 def addSymbolToArray(varName, ifArray, capacity, lineNumb):
-    # ta metoda nie "dokłada" kodu, jest potrzebna tylko do działania kompilatora, dlatego nic w niej nie zwracam
     global symbols_array, first_free_mem_index
     j = None
     for i in range(len(symbols_array)):
@@ -26,32 +25,32 @@ def addSymbolToArray(varName, ifArray, capacity, lineNumb):
         print("Error: Redeclaration of variable {} in line {}.".format(varName, lineNumb))
     else:
         symbols_array.append([varName, ifArray, capacity, first_free_mem_index, False])
-        print(symbols_array[len(symbols_array)-1])
+        #print(symbols_array[len(symbols_array)-1])
         if ifArray:
             first_free_mem_index += capacity
         else:
             first_free_mem_index += 1
 
-# do rejestrów nie można wpisać wartości po prostu podając liczbę, trzeba ją "wygenerować" w rejestrze
 def loadValueToRegister(regName, value):
+    global curr_line_in_code
     code = ''
     while (value > 0):
-        print("value: ", value)
         if (value % 2 == 0):
             code = 'SHL {}\n'.format(regName) + code
             value /= 2
         else:
             code = 'INC {}\n'.format(regName) + code
             value -= 1
-    code = 'RESET {}\n'.format(regName) + code
+    code = 'RST {}\n'.format(regName) + code
+    curr_line_in_code += len(code.split('\n'))-1
     return code
 
 def loadVariableToRegister(varName, regName, lineNumb, onlyAddress):
     # najpierw trzeba znaleźć, gdzie w pamięci jest zmienna o danej nazwie i potem odczytać jej wartość
-    global symbols_array
+    global symbols_array, curr_line_in_code
     code = ''
     j = None
-    print("variable: ", varName)
+    #print("variable: ", varName)
     for i in range(len(symbols_array)):
         if symbols_array[i][0] == varName:
             j = i
@@ -60,16 +59,17 @@ def loadVariableToRegister(varName, regName, lineNumb, onlyAddress):
     else:
         #code += self.loadValueToRegister(regName, symbols_array[j][3])
         #code += loadValueToRegister('b', symbols_array[j][3]) + 'LOAD b\nPUT {}\n'.format(regName)
-        print('gdzie indziej: ', symbols_array[j][3], " ", symbols_array[j])
+        #print('gdzie indziej: ', symbols_array[j][3], " ", symbols_array[j])
         code += loadValueToRegister('b', symbols_array[j][3])
         if not onlyAddress:
             code += 'LOAD b\nPUT {}\n'.format(regName)
+            curr_line_in_code += 2
         return code
 
 # można by to zrobić wyżej, ale tablica może być indeksowana zmienną, więc wygodniej będzie zrobić osobną funkcję
 # do rejestru jest wkładany tylko element tablicy; index - pozycja rządanego elementu
 def loadArrayToRegister(arrName, regName, index, lineNumb, onlyAddress):
-    global symbols_array
+    global symbols_array, curr_line_in_code
     code = ''
     j = None
     for i in range(len(symbols_array)):
@@ -86,14 +86,16 @@ def loadArrayToRegister(arrName, regName, index, lineNumb, onlyAddress):
             # tablica indeksowana zmienną
             #code += loadVariableToRegister(index, 'c', lineNumb) + loadValueToRegister('a', symbols_array[j][3]) + 'ADD c\nDEC a\nPUT b\nLOAD b\nPUT {}\n'.format(regName)
             code += loadVariableToRegister(index, 'c', lineNumb, False) + loadValueToRegister('a', symbols_array[j][3]) + 'ADD c\nDEC a\nPUT b\n'
+            curr_line_in_code += 3
         if not onlyAddress:
             code += 'LOAD b\nPUT {}\n'.format(regName)
+            curr_line_in_code += 2
     return code
 
 # funkcja potrzebna, żeby nie sprawdzać czym są val1 i val2 wiele razy
 # w values są trzymane dwójki (val, is_numb) - val to wartość, is_numb to wartość boolowska, jest równa true, jeżeli to jest zwykła liczba
 def loadValuesToRegs(values, regs, lineNumb, onlyAddress):
-    global symbols_array
+    global symbols_array, curr_line_in_code
     code = ''
     for i in range(len(values)):
         print(values[i])
@@ -117,6 +119,7 @@ def loadValuesToRegs(values, regs, lineNumb, onlyAddress):
             # to jest zwykła liczba
             #code += loadValueToRegister(regs[i], int(values[i][0]))
             code += loadValueToRegister(regs[i], int(values[i][0][0][0]))
+        #curr_line_in_code += len(code.split('\n'))
     return code
 
     ### MAIN PART OF THE PARSER ###
@@ -132,7 +135,7 @@ def p_program(p):
         print("tutaj")
         p[0] = p[2] + 'HALT'
     else:
-        procLen = len(p[1].split('\n'))
+        procLen = len(p[1].split('\n')) + 1
         p[0] = 'JUMP {}'.format(procLen) + p[1] + p[2] + 'HALT'
     
 def p_main(p):
@@ -155,13 +158,15 @@ def p_procedures(p):
     elif (len(p) == 9):
         #p[0] = p[1] + p[7] + 'HALT'
         #procedures_list[len(procedures_list)-1][2] = p.parser.token_slice[p.slice[8]].lineno
-        procedures_list.append(p[3][0], p[3][1], p.parser.token_slice[p.slice[8]].lineno)
+        proc_address = len(p[1].split('\n')) + 1
+        procedures_list.append(p[3][0], p[3][1], p.parser.token_slice[p.slice[8]].lineno, proc_address)
         # chyba jeszcze gdzieś na początku musi być jakiś jump do linijki, w której zaczyna się program
         p[0] = p[7]
     else:
         #p[0] = p[1] + p[6] + 'HALT'
         #procedures_list[len(procedures_list)-1][2] = p.parser.token_slice[p.slice[7]].lineno
-        procedures_list.append(p[3][0], p[3][1], p.parser.token_slice[p.slice[7]].lineno)
+        proc_address = len(p[1].split('\n')) + 1
+        procedures_list.append(p[3][0], p[3][1], p.parser.token_slice[p.slice[7]].lineno, proc_address)
         p[0] = p[6]
 
 def p_proc_head(p):
@@ -253,7 +258,7 @@ def p_assign(p):
     code = ''
     codeLen = 0
     j = None
-    print("p[1][0][0]: ", p[1][0][0], " ", p[1])
+    #print("p[1][0][0]: ", p[1][0][0], " ", p[1])
     for i in range(len(symbols_array)):
         #if p[1][0] == symbols_array[i][0]:
         if p[1][0][0][0] == symbols_array[i][0]:
@@ -272,6 +277,7 @@ def p_assign(p):
         else:
             # to jest liczba, zmienna albo element tablicy
             code += loadValuesToRegs((p[3],), ('c',), p.lexer.lineno-1, True) + loadValuesToRegs((p[1],), ('b',), p.lexer.lineno-1, True) + 'GET c\nSTORE b\n'
+        curr_line_in_code += 2
         p[0] = code
 
 def p_if_statement(p):
@@ -280,16 +286,20 @@ def p_if_statement(p):
     code = ''
     # trzeba zapisać to, co mamy condition i commands
     commLen = len(p[4].split('\n'))
-    print("commLen: ", commLen, " curr: ", curr_line_in_code)
-    if p[2] is tuple:
+    print(p[2], " ", p[4], " commLen: ", commLen, " curr: ", curr_line_in_code, " ", type(p[2]))
+    #if p[2] is tuple:
+    #if len(p[2]) > 1:
+    if isinstance(p[2], tuple):
         # w warunku występuje >= lub <= - są tam dwa skoki zamiast jednego, dlatego ten przypadek osobno
-        code += p[2][0] + p[2][1] + '{}\n'.format(curr_line_in_code + 3) + p[2][2] + '{}\n'.format(curr_line_in_code + commLen)
-        curr_line_in_code += 6 + commLen # 6 to długość kodu dla tego warunku
+        #code += p[2][0] + p[2][1] + '{}\n'.format(curr_line_in_code + 3) + p[2][2] + '{}\n'.format(curr_line_in_code + commLen)
+        code += p[2][0][0] + p[2][0][1] + '{}\n'.format(curr_line_in_code - commLen + 2) + p[2][0][2] + '{}\n'.format(curr_line_in_code + 1)
+        #curr_line_in_code += 6 + commLen # 6 to długość kodu dla tego warunku
     else:
         # w warunku nie ma >= ani <=
-        code += p[2] + '{}\n'.format(curr_line_in_code + commLen)
+        #code += p[2] + '{}\n'.format(curr_line_in_code + commLen) - w curr_line_in_code już jest zawarte commLen
+        code += p[2] + '{}\n'.format(curr_line_in_code + 1)
         condLen = len(p[2].split('\n'))
-        curr_line_in_code += condLen + commLen
+        #curr_line_in_code += condLen + commLen
     #p[0] = p[2] + p[4]
     p[0] = code + p[4]
 
@@ -316,16 +326,22 @@ def p_while_loop(p):
     'command : WHILE condition DO commands ENDWHILE'
     global curr_line_in_code
     code = ''
+    condLen = len(p[2].split('\n'))
     commLen = len(p[4].split('\n'))
+    print("WHILE curr ", curr_line_in_code, " commlen ", commLen)
     # potrzebuję zapisać to, co jest w condition i commands, trzeba mieć adres skoku???
     if p[2] is tuple:
-        code += p[2][0] + p[2][1] + '{}\n'.format(curr_line_in_code + 3) + p[2][2] + '{}\n'.format(curr_line_in_code + 3 + commLen + 1) + p[4] \
+        #code += p[2][0] + p[2][1] + '{}\n'.format(curr_line_in_code + 3) + p[2][2] + '{}\n'.format(curr_line_in_code + 3 + commLen + 1) + p[4] \
+                #+ 'JUMP {}\n'.format(curr_line_in_code)
+        code += p[2][0] + p[2][1] + '{}\n'.format(curr_line_in_code - commLen + 2) + p[2][2] + '{}\n'.format(curr_line_in_code + 1) + p[4] \
                 + 'JUMP {}\n'.format(curr_line_in_code)
-        curr_line_in_code += 6 + commLen + 1
+        #curr_line_in_code += 6 + commLen + 1
     else:
-        condLen = len(p[2].split('\n'))
-        code += p[2] + '{}\n'.format(curr_line_in_code + condLen + commLen) + p[4] + 'JUMP {}\n'.format(curr_line_in_code)
-        curr_line_in_code += condLen + commLen + 1
+        print(" ", condLen)
+        #code += p[2] + '{}\n'.format(curr_line_in_code + condLen + commLen) + p[4] + 'JUMP {}\n'.format(curr_line_in_code)
+        code += p[2] + '{}\n'.format(curr_line_in_code + 2) + p[4] + 'JUMP {}\n'.format(curr_line_in_code- condLen - commLen + 2)
+        #curr_line_in_code += condLen + commLen + 1
+    curr_line_in_code += 1
     p[0] = code
 
 def p_repeat_loop(p):
@@ -346,37 +362,45 @@ def p_repeat_loop(p):
 
 def p_equal(p):
     'condition : value EQUAL value'
+    global curr_line_in_code
     code = loadValuesToRegs([p[1], p[3]], ['b', 'd'], False)
     code += 'GET b\nSUB d\nPUT c\nGET d\nSUB b\nADD c\nJPOS '
+    curr_line_in_code += 7
     p[0] = code
     
 def p_negation(p):
     'condition : value NEG value'
+    global curr_line_in_code
     code = loadValuesToRegs([p[1], p[3]], ['b', 'd'], False)
     code += 'GET b\nSUB d\nPUT c\nGET d\nSUB b\nADD c\nJZERO '
+    curr_line_in_code += 7
     p[0] = code
 
 def p_greater_less(p):
     '''condition : value GREATER value
                     | value LESS value'''
+    global curr_line_in_code
     code = ''
     if p[2] == ">":
         code += loadValuesToRegs([p[1], p[3]], ['b', 'd'], p.lexer.lineno-1, False)
     else:
         code += loadValuesToRegs([p[1], p[3]], ['d', 'b'], p.lexer.lineno-1, False)
     code += 'GET b\nSUB d\nJZERO '
+    curr_line_in_code += 3
     p[0] = code
 
 def p_greater_less_equal(p):
     '''condition : value GREATEREQ value
                     | value LESSEQ value'''
+    global curr_line_in_code
     code = ''
     # te dwa rodzaje warunków są takie same, są własną odwrotnością, wystarczy wstawić wartości do rejestrów odwrotnie
     if p[2] == ">=":
-        code += loadValuesToRegs([p[1], p[3]], ['b', 'd'], False)
+        code += loadValuesToRegs([p[1], p[3]], ['b', 'd'], p.lexer.lineno, False)
     else:
-        code += loadValuesToRegs([p[1], p[3]], ['d', 'b'], False)
-    p[0] = (code, 'GET b\nSUB d\nJPOS ', '\nGET d\nSUB b\nJPOS ')
+        code += loadValuesToRegs([p[1], p[3]], ['d', 'b'], p.lexer.lineno, False)
+    p[0] = ((code, 'GET b\nSUB d\nJPOS ', 'GET d\nSUB b\nJPOS '),)
+    curr_line_in_code += 6
 
 ### EXPRESSIONS ###
 
@@ -387,11 +411,13 @@ def p_expression_value(p):
 def p_add_sub(p):
     '''expression : value PLUS value
                     | value MINUS value'''
+    global curr_line_in_code
     code = loadValuesToRegs([p[1], p[3]], ['b', 'd'], p.lexer.lineno-1, False)
     if p[2] == '+':
         code += 'GET b\nADD d\nPUT f\n'
     else:
         code += 'GET b\nSUB d\nPUT f\n'
+    curr_line_in_code += 3
     p[0] = ('f', code)
 
 def p_multiply(p):
@@ -407,9 +433,13 @@ def p_divide(p):
     code = loadValuesToRegs([p[1], p[3]], ['b', 'd'], False)
     #code += 'RST c\nRST f\nGET d\nPUT e\nSHL e\nGET b\nJZERO  \nSUB e\nJZERO  \nINC c\nJUMP  \nGET e\nJZERO  \nSUB b\nJZERO  \nSHL c\nGET c\nADD f\nPUT f\nRST c\
         #\nSHR e\nGET b\nSUB e\nSHL a\nPUT e\nJUMP  \n'
-    code += 'RST e\nRST c\nGET d\nPUT c\nGET b\nSUB c\nJZERO 9\nINC e\nJPOS \nGET c\nSUB b\nJPOS \nINC e\nJUMP \nSHL c\nGET b\nSUB c\
-        \nJZERO \nINC e\nINC e\nJPOS\nSHR c\nGET b\nSUB c\nPUT b\nJUMP \n'
-    p[0] = ('e', code)
+    #code += 'RST e\nRST c\nGET d\nPUT c\nGET b\nSUB c\nJZERO 9\nINC e\nJPOS \nGET c\nSUB b\nJPOS \nINC e\nJUMP \nSHL c\nGET b\nSUB c\
+        #\nJZERO \nINC e\nINC e\nJPOS\nSHR c\nGET b\nSUB c\nPUT b\nJUMP \n'
+    code += 'GET d\nPUT e\nGET b\nPUT c\nGET b\nJZERO \nGET d\nJZERO \nGET b\nSUB e\nPUT b\nGET c\nSUB d\nADD b\nJZERO \nGET d\nSUB c\nJZERO \nJUMP \nRST g\n\
+        JUMP \nRST g\nINC g\nJUMP \nGET d\nPUT b\nGET c\nPUT e\nRST g\nRST f\nINC f\nGET e\nJZERO \nSHR e\nSHR b\nJUMP \nGET c\nPUT e\nGET b\nJZERO \n\
+        SHR b\nSHL e\nINC f\nJUMP \nGET d\nPUT b\nGET f\nJZERO \nINC b\n GET b\nSUB d\nPUT b\nJZERO \nDEC b\nDEC f\nSHL g\nINC g\nSHR e\nGET b\nPUT d\n\
+        JUMP \nGET d\nPUT b\nSHL g\nDEC f\nSHR e\nJUMP \nGET g\n'
+    p[0] = ('g', code)
 
 def p_modulo(p):
     'expression : value MOD value'
@@ -422,17 +452,20 @@ def p_read(p):
     code = ''
     j = None
     for i in range(len(symbols_array)):
-        if p[2][0][0] == symbols_array[i][0]:
+        #if p[2][0][0] == symbols_array[i][0]:
+        if p[2][0][0][0] == symbols_array[i][0]:
             j = i
     if j == None:
         raise Exception("Error: Usage of undeclared variable {} in line {}.".format(p[2][0], p.lexer.lineno-1))
     else:
-        if p[2][0][1]:
+        #print(p[2])
+        if p[2][1]:
             # to jest tablica
-            if type(p[2][0][2]) is int:
-                code += 'READ\n' + loadValueToRegister('b', symbols_array[j][3]+p[2][0][2]-1) + 'STORE b\n'
+            if type(p[2][2]) is int:
+                #code += 'READ\n' + loadValueToRegister('b', symbols_array[j][3]+p[2][0][2]-1) + 'STORE b\n'
+                code += 'READ\n' + loadValueToRegister('b', symbols_array[j][3]+p[2][2]) + 'STORE b\n'
             else:
-                code += loadVariableToRegister(p[2][0][2], 'a', p.lexer.lineno-1, False) + loadValueToRegister('b', symbols_array[j][3]) + 'ADD b\nDEC a\nPUT b\nREAD\nSTORE b\n'
+                code += 'READ\n' + loadVariableToRegister(p[2][2], 'a', p.lexer.lineno-1, False) + loadValueToRegister('b', symbols_array[j][3]) + 'ADD b\nDEC a\nPUT b\nREAD\nSTORE b\n'
         else:
             # to jest zmienna
             code += 'READ\n' + loadValueToRegister('b', symbols_array[j][3]) + 'STORE b\n'
@@ -443,14 +476,11 @@ def p_read(p):
 def p_write(p):
     'command : WRITE value SEMICOLON'
     code = ''
-    # jeżeli to jest liczba, to trzeba ją po prostu wypisać, jeżeli zmienna, to trzeba znaleźć jej adres w pamięci i wypisać jej wartość,
-    # jeżeli jest to indeks w tablicy, to mamy dwie możliwości: albo jako indeks jest liczba, albo zmienna
-    # żeby wypisać wartość to musi się ona znaleźć w rejestrze a, bo tylko wtedy zadziała WRITE z komend assemblerowych
     #if p[2][1]:
     #if p[2][0] is not tuple:
     if not isinstance(p[2][0], tuple):
         # to jest liczba
-        print(p[2], " ", type(p[2][0]))
+        #print(p[2], " ", type(p[2][0]))
         code += loadValueToRegister('a', p[2][0]) + 'WRITE\n'
     else:
         # to jest albo zmienna albo element tablicy
@@ -493,6 +523,7 @@ def p_array_identifier(p):
 ### SYNTAX ERRORS ###
 
 def p_error(p):
+    print(p)
     raise SyntaxError("Error: Syntax error in line {}.".format(p.lexer.lineno-1))
 
 programme = ''
